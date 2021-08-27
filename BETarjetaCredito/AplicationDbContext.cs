ea31ad6ebb2c10567ad5bdeb8c4267c85330ed32
Sergio Chamorro <sergiochamo96@gmail.com>
2021-08-27 19:49:31 -0400

    Agregar archivos de proyecto.

---
+++ BETarjetaCredito/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BETarjetaCredito.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BETarjetaCredito
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
