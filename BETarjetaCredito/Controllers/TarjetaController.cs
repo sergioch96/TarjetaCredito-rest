09d9be298e1e44773a4dee73ff5d4b7c900e79be
Sergio Chamorro <sergiochamo96@gmail.com>
2021-09-26 16:39:39 -0400

    Se agrego SqlServerUtil para las conexiones y un metodo nuevo para guardar imagenes

--- BETarjetaCredito/Controllers/TarjetaController.cs
+++ BETarjetaCredito/Controllers/TarjetaController.cs
@@ -1,10 +1,11 @@
 ﻿using BETarjetaCredito.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data;
 using System.Threading.Tasks;
+using System.IO;
+using BETarjetaCredito.Util;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,18 +17,45 @@ namespace BETarjetaCredito.Controllers
     {
         private readonly AplicationDbContext _context;
 
+        //private static string _connStringSqlServer;
+
+        /// <summary>
+        /// recupera todas las tarjetas de creditos
+        /// </summary>
+        private const string QRY_SELECT_TARJETAS = "select Id, Titular, NumeroTarjeta, FechaExpiracion, CVV from TarjetaCredito"; 
+
         public TarjetaController(AplicationDbContext context)
         {
             _context = context;
+
+            //_connStringSqlServer = config.GetConnectionString("DevConnection");
         }
 
         // GET: api/<TarjetaController>
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public IActionResult Get()
         {
             try
             {
-                var listTarjetas = await _context.TarjetaCredito.ToListAsync();
+                var listTarjetas = new List<TarjetaCredito>();
+                //var constr = configuration.GetConnectionString("DevConnection");
+                //var result = new DataSet();
+
+                var datos = SqlServerUtil.ExecuteQueryDataSet(QRY_SELECT_TARJETAS);
+
+                foreach (DataRow item in datos.Tables[0].Rows)
+                {
+                    listTarjetas.Add(new TarjetaCredito
+                    {
+                        Id = int.Parse(item["Id"].ToString()),
+                        Titular = item["Titular"].ToString(),
+                        NumeroTarjeta = item["NumeroTarjeta"].ToString(),
+                        FechaExpiracion = item[3].ToString(),
+                        CVV = item[4].ToString()
+                    });
+                }
+
+                //var listTarjetas = await _context.TarjetaCredito.ToListAsync();
                 return Ok(listTarjetas);
             }
             catch (Exception ex)
@@ -89,6 +117,27 @@ namespace BETarjetaCredito.Controllers
                 _context.TarjetaCredito.Remove(tarjeta);
                 await _context.SaveChangesAsync();
                 return Ok(new { message = "La tarjeta fue eliminada con éxito!" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<TarjetaController>
+        [HttpPost("saveFile")]
+        public ActionResult SaveFile([FromForm] FileModel file)
+        {
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "Files", file.FormFile.FileName);
+
+                using (Stream stream = new FileStream(path, FileMode.Create))
+                {
+                    file.FormFile.CopyTo(stream);
+                }
+
+                return Ok(new { message = "La imagen fue agregada con éxito!", archivo = file.FormFile.FileName });
             }
             catch (Exception ex)
             {
